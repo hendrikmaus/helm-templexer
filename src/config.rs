cecd15004e70f017278e5884025aee40765399fb
Hendrik Maus <hendrik.maus@trivago.com>
2021-08-23 14:36:15 +0200

    add comment

--- src/config.rs
+++ src/config.rs
@@ -96,6 +96,7 @@ impl Config {
                 )
             })?;
 
+            // if we're already next to the config file, the base path will be empty
             if !base_path.to_string_lossy().is_empty() {
                 log::trace!("changing base path for execution to {:?}", base_path);
                 std::env::set_current_dir(base_path)?;
